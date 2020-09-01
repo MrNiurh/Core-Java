d4dbdf817865435eee5ac03e85e90179768f0944
MrNiurh <35952284+MrNiurh@users.noreply.github.com>
2020-09-01 22:20:20 +0800

    Hello World!

---
+++ src/Chapter3/HelloWorld.java
@@ -0,0 +1,13 @@
+package Chapter3;
+
+/**
+ * @Author:MrNiurh
+ * @Date:Creat in 2020/9/1
+ * @Github:https://github.com/MrNiurh
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
