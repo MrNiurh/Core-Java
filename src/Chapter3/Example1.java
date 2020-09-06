271962e0e3fa3834bd3fa0c2687d8a8f37ebd2e9
MrNiurh <35952284+MrNiurh@users.noreply.github.com>
2020-09-06 23:10:00 +0800

    2020.9.6 commit

---
+++ src/Chapter3/Example1.java
@@ -0,0 +1,51 @@
+package Chapter3;
+
+/**
+ * @Author:ONE
+ * @Date:Creat in 2020/9/6
+ * @Github:https://github.com/MrNiurh
+ */
+public class Example1 {
+
+    public static void main(String[] args) {
+        nanTest();
+        doubleTest();
+
+    }
+
+    /**
+     * @Description 用于检测一个特定值是否为 NaN用于检测一个特定值是否为 NaN
+     * @Param []
+     * @Return void
+     */
+    private static void nanTest() {
+        double num = Double.NaN;
+
+        if (num == Double.NaN) {
+            System.out.println("==:I can test it correctly");
+        } else {
+            System.out.println("==:I can not test it correctly");
+        }
+
+        if (Double.isNaN(num)) {
+            System.out.println("isNaN(): I can test it correctly");
+        } else {
+            System.out.println("isNaN(): I can not test it correctly");
+        }
+
+        System.out.println("=======================================");
+    }
+
+    /**
+     * @Description 浮点数值不适用于无法接受舍入误差的计算, 计算 （2.0 - 1.1） 的值
+     * @Param []
+     * @Return void
+     **/
+    private static void doubleTest() {
+        System.out.println((2.0 - 1.1));
+    }
+
+}
+
+
+
