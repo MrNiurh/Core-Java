001851d78bdff911b01883fbbc3c9b47320f74a9
MrNiurh <35952284+MrNiurh@users.noreply.github.com>
2020-10-28 22:36:48 +0800

    5.2.5 相等方法测试 Java 核心技术 卷I

---
+++ src/Chapter5/EqualsTest.java
@@ -0,0 +1,32 @@
+package Chapter5;
+
+/**
+ * @author MrNiurh
+ * @date Creat in 2020/10/28
+ * @see <a href="https://github.com/MrNiurh?tab=repositories">github</a>
+ */
+public class EqualsTest {
+
+    /**
+     * 相等测试
+     *
+     * @param args
+     * @return void
+     */
+    public static void main(String[] args) {
+        var alice1 = new Employee("Alice", 75000, 1987, 12, 15);
+        var alice2 = alice1;
+        var alice3 = new Employee("Alice", 75000, 1987, 12, 15);
+        var bob = new Employee("Bob", 50000, 1989, 10, 1);
+
+        System.out.println("alice1 == alice2:" + (alice1 == alice2));
+        System.out.println("alice1 == alice3:" + (alice1 == alice3));
+
+        System.out.println("alice1.equals(alice3):" + alice1.equals(alice3));
+        System.out.println("alice1.equals(bob):" + alice1.equals(bob));
+
+        System.out.println("bob.toString():" + bob);
+    }
+
+
+}
