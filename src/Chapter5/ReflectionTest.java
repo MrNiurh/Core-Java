e042f84160931b3e0d5c1454d1d58dea2f71935c
MrNiurh <niu.rh@foxmail.com>
2020-11-08 15:20:22 +0800

    5.7 反射 Java 核心技术 卷I

--- src/Chapter5/ReflectionTest.java
+++ src/Chapter5/ReflectionTest.java
@@ -1,5 +1,8 @@
 package Chapter5;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.Scanner;
 
@@ -48,29 +51,99 @@ public class ReflectionTest {
 
     /**
      * Prints all constructors of a class
+     * 类中的所有构造器
      *
      * @param cl
      * @return void
      */
     private static void printConstructors(Class cl) {
+
+        Constructor[] constructors = cl.getConstructors();
+
+        for (Constructor c : constructors) {
+            String name = c.getName();
+            System.out.print("  ");
+            String modifiers = Modifier.toString(c.getModifiers());
+            if (modifiers.length() > 0) {
+                System.out.print(modifiers + "  ");
+            }
+            System.out.print(name + "(");
+
+            // print parameters types
+            Class[] parameterType = c.getParameterTypes();
+
+            for (int i = 0; i < parameterType.length; i++) {
+                if (i > 0) {
+                    System.out.print(",");
+                }
+                System.out.print(parameterType[i].getName());
+            }
+            System.out.println(");");
+        }
+
+
     }
 
     /**
      * Prints all methods of a class
+     * 类中的所有方法
      *
      * @param cl
      * @return void
      */
     private static void printMethods(Class cl) {
+
+        Method[] methods = cl.getDeclaredMethods();
+
+        for (Method m : methods) {
+            Class retType = m.getReturnType();
+            String name = m.getName();
+
+            System.out.print("  ");
+
+            // print modifiers,return type and method name
+            String modifiers = Modifier.toString(m.getModifiers());
+            if (modifiers.length() > 0) {
+                System.out.print(modifiers + " ");
+            }
+            System.out.print(retType.getName() + " " + name + "(");
+
+            // print parameter types
+            Class[] paramTypes = m.getParameterTypes();
+
+            for (int i = 0; i < paramTypes.length; i++) {
+                if (i > 0) {
+                    System.out.print(", ");
+                }
+                System.out.print(paramTypes[i].getName());
+            }
+            System.out.println(");");
+
+        }
     }
 
 
     /**
      * Prints all fields of a class
+     * 打印类的所有字段
      *
      * @param cl
      * @return void
      */
     private static void printFields(Class cl) {
+
+        Field[] fields = cl.getDeclaredFields();
+
+        for (Field f : fields) {
+            Class type = f.getType();
+            String name = f.getName();
+            System.out.print("  ");
+            String modifiers = Modifier.toString(f.getModifiers());
+
+            if (modifiers.length() > 0) {
+                System.out.print(modifiers + " ");
+            }
+            System.out.println(type.getName() + " " + name + ";");
+        }
     }
 }
