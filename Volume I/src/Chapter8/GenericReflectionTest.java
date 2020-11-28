387ef70f3ea5c52bbb97b697831dcd9a78e9e17f
MrNiurh <niu.rh@foxmail.com>
2020-11-28 16:22:47 +0800

    8.9.3 虚拟中的泛型类型信息 Java 核心技术 卷I

--- Volume I/src/Chapter8/GenericReflectionTest.java
+++ Volume I/src/Chapter8/GenericReflectionTest.java
@@ -1,6 +1,7 @@
 package Chapter8;
 
-import java.lang.reflect.Method;
+import java.lang.reflect.*;
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -40,9 +41,80 @@ public class GenericReflectionTest {
 
 
     private static void printClass(Class<?> cl) {
+        System.out.print(cl);
+        printTypes(cl.getTypeParameters(), "<", ",", ">", true);
+        Type sc = cl.getGenericSuperclass();
+        if (sc != null) {
+            System.out.print(" extends ");
+            printType(sc, false);
+        }
+        printTypes(cl.getGenericInterfaces(), " implements ", ",", ",", false);
+        System.out.println();
     }
 
+
     private static void printMethod(Method m) {
+
+        String name = m.getName();
+        System.out.print(Modifier.toString(m.getModifiers()));
+        System.out.print("  ");
+        printTypes(m.getTypeParameters(), "<", ",", ">", true);
+
+        printType(m.getGenericReturnType(), false);
+        System.out.print(" ");
+        System.out.print(name);
+        System.out.print("(");
+        printTypes(m.getGenericParameterTypes(), "", "", "", false);
+        System.out.println(")");
     }
+
+    private static void printTypes(Type[] types, String pre, String sep, String suf, boolean isDefinition) {
+
+        if (pre.equals(" extends ") && Arrays.equals(types, new Type[]{Object.class}))
+            return;
+        if (types.length > 0)
+            System.out.print(pre);
+        for (int i = 0; i < types.length; i++) {
+            if (i > 0)
+                System.out.print(sep);
+            printType(types[i], isDefinition);
+        }
+        if (types.length > 0)
+            System.out.print(suf);
+    }
+
+    private static void printType(Type type, boolean isDefinition) {
+
+        if (type instanceof Class) {
+            var t = (Class<?>) type;
+            System.out.print(t.getName());
+        } else if (type instanceof TypeVariable) {
+            var t = (TypeVariable<?>) type;
+            System.out.print(t.getName());
+            if (isDefinition)
+                printTypes(t.getBounds(), " extends ", " & ", "", false);
+
+        } else if (type instanceof WildcardType) {
+            var t = (WildcardType) type;
+            System.out.print("?");
+            printTypes(t.getUpperBounds(), " extends ", " & ", "", false);
+            printTypes(t.getLowerBounds(), " super ", " & ", "", false);
+        } else if (type instanceof ParameterizedType) {
+            var t = (ParameterizedType) type;
+            Type owner = t.getOwnerType();
+            if (owner != null) {
+                printType(owner, false);
+                System.out.print(".");
+            }
+            printType(t.getRawType(), false);
+            printTypes(t.getActualTypeArguments(), "<", ",", ">", false);
+        } else if (type instanceof GenericArrayType) {
+            var t = (GenericArrayType) type;
+            System.out.print("");
+            printType(t.getGenericComponentType(), isDefinition);
+            System.out.print("[]");
+        }
+    }
+
 
 }
